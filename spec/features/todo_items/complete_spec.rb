9a5264573640a7e3e4a60d2459a3ce5af628cece
Eric Zell <ezell@rbmtechnologies.com>
2016-01-07 16:28:07 -0500

    Add completed at times to completed todo items

--- spec/features/todo_items/complete_spec.rb
+++ spec/features/todo_items/complete_spec.rb
@@ -11,7 +11,27 @@ describe "Completing a todo item" do
       click_link "Mark Complete"
     end
     todo_item.reload
+    expect(page).to have_content("Item marked as complete")
     expect(todo_item.completed_at).to_not be_nil
   end
 
+  context "with completed items" do
+    let!(:completed_todo_item) { todo_list.todo_items.create(content: "Eggs", completed_at: 5.minutes.ago) }
+
+    it "shows the completed items as complete" do
+      visit_todo_list(todo_list)
+      within dom_id_for(completed_todo_item) do
+        expect(page).to have_content(completed_todo_item.completed_at)
+      end
+
+    end
+
+    it "does not give the option to mark complete" do
+      visit_todo_list(todo_list)
+      within dom_id_for(completed_todo_item) do
+        expect(page).to_not have_content("Mark Complete")
+      end
+    end
+
+  end
 end
